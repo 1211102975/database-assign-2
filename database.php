ad8b9810f15c0faac4fbc55fe3a6c01cfba7d661
Xinyi <1211102975@student.mmu.edu.my>
2025-02-10 01:00:24 +0800

    edit endpoint

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 
 // Create connection
-$database = mysqli_connect("applem.cidfrofigwko.us-east-1.rds.amazonaws.com","applem","abc12345678","AppleMart");
+$database = mysqli_connect("applem.cdqmpduwjecg.us-east-1.rds.amazonaws.com","applem","abc12345678","AppleMart");
 
 // Check connection
 if (mysqli_connect_errno()) {
